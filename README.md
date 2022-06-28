109f71eeb22bff33ca09ea2a7032c77d56f414b8
Luiz Correa <luiz.correa@laborit.com.br>
2022-06-28 09:01:42 -0300

    start commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## Ignite - Course - Module One
+
+---
+
+This project is about to create a structure with prisma ORM.
+
+--
+
+### Links
+
+- UML Diagram - plant.puml file.
---
+++ server.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient()
+
+async function main() {
+    const result = await prisma.courses.create({
+        data: {
+            name: 'Curso de Nodejs',
+            duration: 200,
+            description: 'Curso excelente de nodejs'
+        }
+    })
+}
+
+main()
